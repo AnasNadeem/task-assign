2f5d9698056c5bd2b08a637fb3021fdbdbc18244
Anas Nadeem <anas5678go@gmail.com>
2022-01-20 17:42:21 +0530

    Models created

--- taskapp/models.py
+++ taskapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+TASK_STATUS = [
+    ('Moderate', 'Moderate'),
+    ('Urgent', 'Urgent'),
+]
+
+TASK_PROGRESS = [
+    ('Pending', 'Pending'),
+    ('Working', 'Working'),
+    ('Completed', 'Completed'),
+]
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=8, choices=TASK_STATUS, default='Moderate')
+    progress = models.CharField(max_length=12, choices=TASK_PROGRESS, default='Pending')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    assigned_to = models.ManyToManyField(User, blank=True, related_name='assigned')
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
